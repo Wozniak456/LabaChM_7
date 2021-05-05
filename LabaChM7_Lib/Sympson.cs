b50a872857c139f88016d00a410c89b18515af98
Sofiia <65614017+Wozniak456@users.noreply.github.com>
2021-05-05 11:35:12 +0300

    Add project files.

---
+++ LabaChM7_Lib/Sympson.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LabaChM7_Lib
+{
+    public class Sympson
+    {
+        public double Y(double x)
+        {
+            return Math.Cos(x) / (x + 1);
+        }
+        public double SympsonMethod(double a, double b, int n)
+        {
+            double x, h, s;
+            h = (b - a) / n;
+            s = 0; x = a + h;
+            while (x < b)
+            {
+                s += 4 * Y(x);
+                x += h;
+                s += 2 * Y(x);
+                x += h;
+            }
+            s = h / 3 * (s + Y(a) - Y(b));
+            return s;
+        }
+        public double gauss(double a, double b, int N )
+        {
+            double x, rez = 0;
+            double[] Xi = { -0.7745967, 0, 0.7745967 };
+            double[] Ai = { 0.5555556, 0.8888889, 0.5555556 };
+            for (int i = 0; i < 3; i++)
+            {
+                x = (a + b) / 2 + Xi[i] * (b - a) / 2;
+                rez += Ai[i] * Y(x);
+            }
+            return rez * (b - a) / 2;
+        }
+
+    }
+}
---
+++ LabaChM_7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LabaChM7_Lib;
+namespace LabaChM_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вiдрiзок iнтегрування[a, b] —> (a) = ");
+            double a = int.Parse(Console.ReadLine()); //2
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine()); //5
+            Console.Write("Eps = ");
+            double eps = double.Parse(Console.ReadLine()); // 0,0001
+            Sympson val = new Sympson();
+            int n = 2; double rez; int i = 0;
+            do
+            {
+                rez = Math.Abs(val.SympsonMethod(a, b, n * i) - val.SympsonMethod(a, b, n * (i + 1)));
+                i++;
+            } while (rez > eps);
+            Console.WriteLine($"\nIntegral Value with eps: {eps} = {val.SympsonMethod(a, b, n * (i))}");
+            Console.WriteLine($"Count of iterations: {i}");
+            Console.WriteLine("\n~ Gauss method ~");
+            int N = 100; double s = 0;
+            for (int j = 0; j < N; j++)
+            {
+                s += val.gauss(a + j * (b - a) / N, a + (j + 1) * (b - a) / N, N);
+            }
+            Console.WriteLine($"Integral value = {s}");
+        }
+    }
+}
